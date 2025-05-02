9573d1d69c590123765a70ce572bd438db07cf35
nurrbb <enurb320@gmail.com>
2025-05-02 13:23:57 +0300

    feat(task): define DTO for task creation and updates

--- src/main/java/com/nurbb/taskmanagerapp/model/dto/request/TaskRequest.java
+++ src/main/java/com/nurbb/taskmanagerapp/model/dto/request/TaskRequest.java
@@ -1,10 +1,10 @@
 package com.nurbb.taskmanagerapp.model.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskRequest {
     private String title;
     private String description;
