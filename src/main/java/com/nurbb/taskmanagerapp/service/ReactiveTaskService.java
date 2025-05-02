6eeee9ca1af51babb43a82ee52b758c0d52119ef
nurrbb <enurb320@gmail.com>
2025-05-02 13:24:40 +0300

    feat(task-reactive): implement reactive service layer for tasks

--- src/main/java/com/nurbb/taskmanagerapp/service/ReactiveTaskService.java
+++ src/main/java/com/nurbb/taskmanagerapp/service/ReactiveTaskService.java
@@ -12,7 +12,14 @@ import java.util.UUID;
 
 @Service
 public class ReactiveTaskService {
+
     private final Map<UUID, Task> taskStore = new HashMap<>();
+
+// Multi-threading ile eşzamanlı tasklar için Thread sınıfı kullanılıyor. İşi tanımlamak için runnable interface
+// verilir void run() metodunu barındıran bir interface yalnızca side-effect oluşturmak amacıyla kullanılıyor
+// değer döndürmez ve checked excep fırlatamaz "Sonuç üreten ve hata yönetimi gerektiren işlemler için yetersiz.
+// Java 5 ile birlikte Callable<T> interface tanıtıldı. İş tamamlandığında sonuç döndürebilir hata fırlatabilir
+//  Reaktif'te ise klasik senkron işlemleri rekatif lazy ve non-blocking yapılara sarmak için kullanılır
 
     public Mono<Task> createTask(String title, String description){
         return Mono.fromCallable(() -> {
@@ -27,13 +34,20 @@ public class ReactiveTaskService {
             return task;
         });
     }
+
+    // Flux: çok sayıda task nesnesini döndürür. Project Reactor lib ait bir Publisher interface'idir
+    // fromItetable ise herhangi bir iterable veri kaynağını Flux'a döndürür. TaskStore hash map olarak tutuyorduk
+
     public Flux<Task> getAllTasks(){
         return Flux.fromIterable(taskStore.values())
                 .delayElements(Duration.ofSeconds(1));
     }
+
     public Mono<Task> getTaskById(UUID id){
         return Mono.justOrEmpty(taskStore.get(id));
-    }
+    } // görev bulunamazsa mono.empty() döner hata fırlatılmadan yönetilir.
+
+    //switchIfEmpty bir önceki mono boşsa yine zinciri kırmaz ama hata fırlatır.
     public Mono<Task> updateTaskStatus(UUID id, Task.TaskStatus newStatus){
      return Mono.justOrEmpty(taskStore.get(id))
              .switchIfEmpty(Mono.error(new IllegalArgumentException("Task not found with id: " + id)))
@@ -43,6 +57,7 @@ public class ReactiveTaskService {
                  return updatedTask;
              });
     }
+
     public Mono<Void> deleteTask(UUID id){
         return Mono.fromRunnable(() -> taskStore.remove(id));
     }
