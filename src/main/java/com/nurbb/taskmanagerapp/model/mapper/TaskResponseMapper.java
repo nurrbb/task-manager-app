2fb3d12dea98f697bc8cf24222d2d8388c587216
nurrbb <enurb320@gmail.com>
2025-05-02 13:24:07 +0300

    feat(task): map task entities to response models

--- src/main/java/com/nurbb/taskmanagerapp/model/mapper/TaskResponseMapper.java
+++ src/main/java/com/nurbb/taskmanagerapp/model/mapper/TaskResponseMapper.java
@@ -7,11 +7,15 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+// Mapper iki nesne türü arasındaki veri çevirisini yapar task -> TaskResponseDto
+
 @Mapper(componentModel = "spring")
 public interface TaskResponseMapper {
 
+// İnterface newlenemeyeceğiiçin bir sınıf örneği eldeediyoruz böylece dönüşümü yapabiliyoruz
     TaskResponseMapper INSTANCE = Mappers.getMapper(TaskResponseMapper.class);
 
     TaskResponseDTO toDTO(Task task);
+
     List<TaskResponseDTO> toDTOList(List<Task> tasks);
 }
